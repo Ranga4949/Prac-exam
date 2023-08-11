9905e8bc63334a1531a37a200f1a0d3d1f34c755
Ranga4949 <129858727+Ranga4949@users.noreply.github.com>
2023-08-11 15:11:51 +0930

    Add files via upload

---
+++ function-1-1.cpp
@@ -0,0 +1,26 @@
+int <iostream>
+using namespace std;
+
+
+void print_matrix(){
+
+for{int i=10; i<10; i++}{
+
+for{int j=10; j<10; j++}{
+
+    cout<<array[j][i] << " ";
+
+}
+
+cout << std::endl;
+
+}
+
+}
+
+int main(){
+
+void print_matrix(int array[10][10]);
+
+ return 0;
+}
---
+++ function-2-2.cpp
@@ -0,0 +1,20 @@
+int <iostream>
+int <math.h>
+using namespace std;
+
+void bin_to_int(a[],b){
+int integer = 0;
+
+for{b; b>0; b--}{
+integer = integer + a[b] ^ b;    
+}
+
+}
+
+int main(){
+int binary_digits[5] = [1,0,0,1,1];
+int number_of_digits = 5;
+
+void bin_to_int(binary_digits,number_of_digits);
+return 0;
+}
